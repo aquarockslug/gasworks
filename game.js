1f89245b32d1d20e5e1d8a9f9354e9db72b31bff
aqua <aqua.rock.slug@gmail.com>
2026-02-01 21:30:44 -0600

    shadow

--- game.js
+++ game.js
@@ -94,7 +94,7 @@ function groundLayer() {
 
 function gameInit() {
 	objectDefaultDamping = 0.7;
-	player = new Player(vec2(), vec2(0.5), tile(vec2(), vec2(19, 21), 1));
+	player = new Player(vec2(), vec2(0.5, 0.25), tile(vec2(), vec2(19, 21), 1));
 	player.maskName = MASKS[0];
 	player.drawSize = vec2(1);
 
@@ -158,8 +158,11 @@ function gameUpdate() {
 
 			let tileIndex = typeof pipeTile === "object" ? pipeTile.tile : pipeTile;
 
-			if (tileIndex == pipe(true, "red", 2) || tileIndex == pipe(true, "red", 3))
-				tileIndex = pipe(true, "red", time % 2 > 1 ? 2 : 3)
+			if (
+				tileIndex == pipe(true, "red", 2) ||
+				tileIndex == pipe(true, "red", 3)
+			)
+				tileIndex = pipe(true, "red", time % 2 > 1 ? 2 : 3);
 
 			const data = new TileLayerData(tileIndex);
 			gl.setData(vec2(x, y), data);
@@ -184,102 +187,6 @@ function gameRender() {
 }
 
 function postGameRender() {}
-
-class GameObject extends EngineObject {
-	render() {
-		const drawSize = this.drawSize || this.size;
-		const offset = this.getUp(drawSize.y / 4);
-		const pos = this.pos.add(offset);
-		drawTile(pos, drawSize, this.tileInfo, undefined, undefined, this.mirror);
-	}
-}
-
-class Lever extends GameObject {
-	constructor(...args) {
-		super(...args);
-		this.renderOrder = -500;
-		this.on = true;
-	}
-	toggle() {
-		this.on = !this.on;
-		this.tileInfo = tile(vec2(9, 10), vec2(16)).frame(this.on ? 1 : 0);
-	}
-}
-
-class Mask extends GameObject {
-	constructor(...args) {
-		super(...args);
-		this.renderOrder = -500;
-		this.collideWithTile = true;
-	}
-	update() {
-		let step = ((time * 6) % 8) | 0;
-		this.tileInfo = tile(vec2(0, step < 4 ? 0 : step - 4), vec2(8, 10), 2);
-	}
-}
-
-class Player extends GameObject {
-	constructor(...args) {
-		super(...args);
-		this.lastEmitTime = 0;
-		this.emitInterval = 0.1;
-		this.inGas = false;
-	}
-
-	update() {
-		super.update();
-
-		const gasDataAtPos = gl.getData(this.pos.floor().add(vec2(16)));
-		this.inGas = gasDataAtPos.tile;
-		if (lever.on && this.inGas && this.maskName != "red") this.pos = vec2(0);
-
-		const moveInput = keyDirection().clampLength(1).scale(0.075);
-		this.velocity = this.velocity.add(moveInput);
-		this.mirror = this.velocity.x < 0;
-		this.setCollision();
-
-		// TODO clamp
-		cameraPos = this.pos.add(vec2(0, 2));
-
-		if (moveInput.length() === 0) {
-			this.state = this.idle;
-		} else {
-			this.state = this.walk;
-			this.emitDustParticles();
-		}
-		this.state();
-	}
-
-	emitDustParticles() {
-		if (time - this.lastEmitTime > this.emitInterval) {
-			const feetPos = this.pos.add(vec2(0, 0.4));
-			emitGas(feetPos, particles.dust);
-			this.lastEmitTime = time;
-		}
-	}
-
-	setAnimation(state) {
-		const animations = {
-			idle: { rowOffset: 0, frames: 2, speed: 4 },
-			walk: { rowOffset: 1, frames: 4, speed: 6 },
-		};
-
-		const anim = animations[state];
-		this.tileInfo = tile(
-			vec2(0, MASKS.indexOf(this.maskName) * 2 + anim.rowOffset),
-			vec2(19, 21),
-			1,
-		).frame(((time * anim.speed) % anim.frames) | 0);
-	}
-
-	idle() {
-		this.setAnimation("idle");
-	}
-
-	walk() {
-		this.setAnimation("walk");
-	}
-}
 
 engineInit(gameInit, gameUpdate, null, gameRender, postGameRender, [
 	"pipes.png",
---
+++ objects.js
@@ -0,0 +1,102 @@
+class GameObject extends EngineObject {
+	update() {
+		this.renderOrder = -this.pos.y; // sort by y position
+	}
+	render() {
+		const drawSize = this.drawSize || this.size;
+		const offset = this.getUp(drawSize.y / 4);
+		const pos = this.pos.add(offset);
+		drawTile(pos, drawSize, this.tileInfo, undefined, undefined, this.mirror);
+	}
+}
+
+class Lever extends GameObject {
+	constructor(...args) {
+		super(...args);
+		this.on = true;
+	}
+	toggle() {
+		this.on = !this.on;
+		this.tileInfo = tile(vec2(9, 10), vec2(16)).frame(this.on ? 1 : 0);
+	}
+}
+
+class Mask extends GameObject {
+	constructor(...args) {
+		super(...args);
+	}
+	update() {
+		super.update();
+		let step = ((time * 6) % 8) | 0;
+		this.tileInfo = tile(vec2(0, step < 4 ? 0 : step - 4), vec2(8, 10), 2);
+	}
+}
+
+class Player extends GameObject {
+	constructor(...args) {
+		super(...args);
+		this.lastEmitTime = 0;
+		this.emitInterval = 0.1;
+		this.inGas = false;
+	}
+
+	update() {
+		super.update();
+
+		const gasDataAtPos = gl.getData(this.pos.floor().add(vec2(16)));
+		this.inGas = gasDataAtPos.tile;
+		if (lever.on && this.inGas && this.maskName != "red") this.pos = vec2(0);
+
+		const moveInput = keyDirection().clampLength(1).scale(0.06);
+		this.velocity = this.velocity.add(moveInput);
+		this.mirror = this.velocity.x < 0;
+		this.setCollision();
+
+		// TODO clamp
+		cameraPos = this.pos.add(vec2(0, 2)).add(this.velocity.multiply(vec2(-1)));
+
+		if (moveInput.length() === 0) {
+			this.state = this.idle;
+		} else {
+			this.state = this.walk;
+			this.emitDustParticles();
+		}
+		this.state();
+	}
+
+	emitDustParticles() {
+		if (time - this.lastEmitTime > this.emitInterval) {
+			const feetPos = this.pos.add(vec2(0, -0.3));
+			emitGas(feetPos, particles.dust);
+			this.lastEmitTime = time;
+		}
+	}
+
+	setAnimation(state) {
+		const animations = {
+			idle: { rowOffset: 0, frames: 2, speed: 4 },
+			walk: { rowOffset: 1, frames: 4, speed: 6 },
+		};
+
+		const anim = animations[state];
+		this.tileInfo = tile(
+			vec2(0, MASKS.indexOf(this.maskName) * 2 + anim.rowOffset),
+			vec2(19, 21),
+			1,
+		).frame(((time * anim.speed) % anim.frames) | 0);
+	}
+
+	idle() {
+		this.setAnimation("idle");
+	}
+
+	walk() {
+		this.setAnimation("walk");
+	}
+
+	render() {
+		let offset = player.pos.subtract(cameraPos).multiply(vec2(0.15)).add(vec2(0, 0.6))
+		drawTile(this.pos.add(offset), vec2(1), tile(vec2(), vec2(19, 21), 1).frame(2))
+		super.render()
+	}
+}
