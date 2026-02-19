e3d45d1cbc3f0180e91e15b6a40f8e699cb70156
aqua <aqua.rock.slug@gmail.com>
2026-02-18 22:00:58 -0600

    level data comments

--- src/gameData.js
+++ src/gameData.js
@@ -211,11 +211,12 @@ const initTileDataCache = () => {
 
 const level = {
 	pipes: [
+		// a line of pipe tiles with the given position and length
 		...pipeSection(18, 20, 23),
 		...pipeSection(24, 16, 15),
-		{ x: 24, y: 16, value: PIPE_TILES.CORNER_TOP_LEFT },
-		{ x: 25, y: 20, value: pipe("straight", "horizontal", true) },
 		...pipeSection(24, 10, 6, "vertical"),
+
+		// multiple pipe sections connected by corners at the given positions
 		...pipeLine([
 			{ x: 1, y: 18 },
 			{ x: 15, y: 18 },
@@ -225,9 +226,15 @@ const level = {
 			{ x: 3, y: 28 },
 			{ x: 3, y: 23 },
 		]),
+
+		// generates a maze of pipeLines with the given size
 		...mazePattern(7, 4),
 	],
-	gases: [...cloud(24, 17)],
+	gases: [
+		// a cloud of gas at the given position
+		...cloud(24, 17),
+	],
+	valves: [],
 };
 
 const particles = {
